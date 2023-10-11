e56bd136442c09828b7053d070ed8e3d4849b4cb
Alvaro <curielyseguro@gmail.com>
2023-10-11 18:34:18 +0000

    Hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".es", ".org"];
+
+  function dominioRandom() {
+    for (let p = 0; p < pronoun.length; p++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          for (let e = 0; e < ext.length; e++) {
+            dominioaleatorio = pronoun[p] + adj[a] + noun[n] + ext[e];
+            console.log(dominioaleatorio);
+          }
+        }
+      }
+    }
+  }
+
+  let dominioaleatorio = dominioRandom();
 };
